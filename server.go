ed51f0eeaa5c2fb65c10a7026e44742813ac9d8e
Ondrej Sika <ondrej@ondrejsika.com>
2018-08-22 07:33:23 +0200

    Symplify Go server

--- server.go
+++ server.go
@@ -1,17 +1,13 @@
 package main
-import (
-  "net/http"
-  "strings"
-)
-func sayHello(w http.ResponseWriter, r *http.Request) {
-  message := r.URL.Path
-  message = strings.TrimPrefix(message, "/")
-  message = "Hello " + message
-  w.Write([]byte(message))
+import "fmt"
+import "net/http"
+
+func index(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello World from Go!\n")
 }
+
 func main() {
-  http.HandleFunc("/", sayHello)
-  if err := http.ListenAndServe(":8080", nil); err != nil {
-    panic(err)
-  }
+    http.HandleFunc("/", index)
+    fmt.Println("Server startded.")
+    http.ListenAndServe(":80", nil)
 }
